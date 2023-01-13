7994d106da57850cebae30058faab4f018036c24
Matt Porritt <mattp@catalyst-au.net>
2023-01-13 13:20:02 +1100

    add checks for authorisation header and valid HTTP methods

--- application/src/Controller/SynapseController.php
+++ application/src/Controller/SynapseController.php
@@ -4,6 +4,7 @@ namespace App\Controller;
 
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Request;
 
 /**
  * API Controller to serve a mock of the Synapse API.
@@ -22,6 +23,115 @@ class SynapseController extends DataController {
                 'error' => 'Unrecognized request'
         ],
         404);
+    }
+
+    /**
+     * Handle Synapse user registration.
+     *
+     * @Route("/users/{userID}", name="registerUser")
+     */
+    public function registerUser(string $serverID, string $userID, Request $request): JsonResponse
+    {
+        // Check call auth.
+        $authCheck = $this->checkAuth($request);
+        if (!$authCheck['status']) {
+            // Auth check failed, return error info.
+            return $authCheck['message'];
+        }
+
+        // Check HTTP method is accepted.
+        $method = $request->getMethod();
+        $methodCheck = $this->checkMethod(['PUT', 'GET'], $method);
+        if (!$methodCheck['status']) {
+            // Method check failed, return error info.
+            return $methodCheck['message'];
+        }
+
+
+        if ($method == 'PUT') {
+            // Create user.
+        } elseif ($method == 'GET') {
+            // Get user.
+        }
+
+        return new JsonResponse((object) [
+                'severID' => $serverID,
+                'userID' => $userID,
+                'method' => $request->getMethod(),
+                'authHeader' => $request->headers->get('authorization')
+        ]);
+    }
+
+    /**
+     * Check if a correct Authorization header has been received.
+     *
+     * @param Request $request
+     * @return array $response
+     */
+    private function checkAuth(Request $request): array
+    {
+        $response = ['status' => true, 'message' => ''];
+
+        // Check auth key is valid.
+        $authHeader = $request->headers->get('authorization');
+        if (empty($authHeader)) {
+            // No valid auth header found.
+            $response['status'] = false;
+            $response['message'] = new JsonResponse((object) [
+                    'errcode' => 'M_MISSING_TOKEN',
+                    'error' => 'Missing access token'
+            ],
+                    401);
+        } else {
+            $authToken = substr($authHeader, 7);
+            if (!$this->isValidAuthToken($authToken)){
+                // Auth token is not valid.
+                $response['status'] = false;
+                $response['message'] = new JsonResponse((object) [
+                        'errcode' => 'M_UNKNOWN_TOKEN',
+                        'error' => 'Invalid access token passed.'
+                ],
+                        401);
+            }
+        }
+
+        return $response;
+    }
+
+    /**
+     * Check if supplied auth token is valid.
+     *
+     * @param string $authToken
+     * @return bool
+     */
+    private function isValidAuthToken(string $authToken): bool
+    {
+        // TODO: check supplied token against list in DB. For now everything is valid.
+        return True;
+    }
+
+    /**
+     * Check if the API endpoint was called with an accepted HTTP method.
+     *
+     * @param array $acceptedTypes
+     * @param string $method
+     * @return array $response
+     */
+    private function checkMethod(array $acceptedTypes, string $method): array
+    {
+        $response = ['status' => true, 'message' => ''];
+
+        if(!in_array($method, $acceptedTypes)) {
+            // Used method is not allowed for this call.
+            $response['status'] = false;
+            $response['message'] = new JsonResponse((object) [
+                    'errcode' => 'M_UNRECOGNIZED',
+                    'error' => 'Unrecognized request'
+            ],
+                    404);
+        }
+
+        return $response;
     }
 
 }
